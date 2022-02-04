a322e23c2a20890e043f6c7bc3b6a04653601c56
Bennett Zhu <bennettzhu.12.19@gmail.com>
2022-02-03 22:45:06 -0500

    Create appointments table and local tests

---
+++ src/main/java/com/virtudoc/web/entity/Appointment.java
@@ -0,0 +1,102 @@
+package com.virtudoc.web.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "appointments")
+public class Appointment {
+    //Primary Key
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int appointmentId;
+
+    //Foreign Key, links appointments table to parent user table
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String patientName;
+
+    @Column(nullable = false)
+    private String symptoms;
+
+    @Column(nullable = false)
+    private String doctorName;
+
+    @Column(nullable = false)
+    private String location;
+    //    todo: convert to Date object
+
+    @Column(nullable = false)
+    private String date;
+
+    @Column(nullable = false)
+    private String reasonForVisit;
+
+    public Appointment() {
+    }
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(String symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getReasonForVisit() {
+        return reasonForVisit;
+    }
+
+    public void setReasonForVisit(String reasonForVisit) {
+        this.reasonForVisit = reasonForVisit;
+    }
+}
