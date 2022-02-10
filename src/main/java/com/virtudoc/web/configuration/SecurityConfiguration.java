a46acd5895b003a168f9f821846de6f7db348fb7
PlasAJulian <julian_aplas@outlook.com>
2022-02-10 18:48:22 -0500

    updated nav bar

--- src/main/java/com/virtudoc/web/configuration/SecurityConfiguration.java
+++ src/main/java/com/virtudoc/web/configuration/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // The following endpoints do not require authentication.
                 .antMatchers("/", "/sampleentity/mail", "/sampleentity/all",
-                        "/login", "/register", "/notifications", "/debug/health","/message","/video","/appointments")
+                        "/login", "/register", "/notifications", "/debug/health","/message","/video","/ApptPage")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
