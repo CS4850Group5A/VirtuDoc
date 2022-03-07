af6e591d9667aab7076d664dbda946c4209a4089
PlasAJulian <julian_aplas@outlook.com>
2022-03-07 10:38:21 -0500

    adding files form my test to main project. also added configs to security to get it running.

--- src/main/java/com/virtudoc/web/configuration/SecurityConfiguration.java
+++ src/main/java/com/virtudoc/web/configuration/SecurityConfiguration.java
@@ -32,9 +32,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 // By default, all requests must have authorization.
                 .authorizeRequests()
                 // The following endpoints do not require authentication.
-                .antMatchers("/", "/img/**",
+                .antMatchers("/", "/img/**","/js/**",
                         "/login", "/register", "/notifications", "/debug/health","/message","/video", "/appointment",
-                        "/admin_records","/notifications/delete/{pathvariable:[0-9A-Za-z]+}")
+                        "/admin_records","/notifications/delete/{pathvariable:[0-9A-Za-z]+}","/chat/{to}","/registration/{userName}","/message/*","/fetchAllUsers","/app/chat/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
---
+++ src/main/resources/static/js/chat.js
@@ -0,0 +1,157 @@
+const url = 'http://localhost:8080';
+let stompClient;
+let selectedUser;
+let newMessages = new Map();
+let $chatHistory;
+let $button;
+let $textarea;
+let $chatHistoryList;
+
+function connectToChat(userName) {
+    console.log("connecting to chat...")
+    let socket = new SockJS(url + '/chat');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log("connected to: " + frame);
+        stompClient.subscribe("/topic/messages/" + userName, function (response) {
+            let data = JSON.parse(response.body);
+            if (selectedUser === data.fromLogin) {
+                render(data.message, data.fromLogin);
+            } else {
+                newMessages.set(data.fromLogin, data.message);
+                $('#userNameAppender_' + data.fromLogin).append('<span id="newMessage_' + data.fromLogin + '" style="color: red">+1</span>');
+            }
+        });
+    });
+}
+
+function sendMsg(from, text) {
+    stompClient.send("/app/chat/" + selectedUser, {}, JSON.stringify({
+        fromLogin: from,
+        message: text
+    }));
+}
+
+function registration() {
+    let userName = document.getElementById("userName").value;
+    if(userName.length != 0 ){
+        var button = document.getElementById('RegBTN');
+        hideButton(button);
+        $.get(url + "/registration/" + userName, function (response) {
+            connectToChat(userName);
+        }).fail(function (error) {
+            if (error.status === 400) {
+                alert("Login is already busy!")
+            }
+        })
+    }
+}
+
+function hideButton(x)
+ {
+  x.style.display = 'none';
+ }
+
+function selectUser(userName) {
+    console.log("selecting users: " + userName);
+    selectedUser = userName;
+    let isNew = document.getElementById("newMessage_" + userName) !== null;
+    if (isNew) {
+        let element = document.getElementById("newMessage_" + userName);
+        element.parentNode.removeChild(element);
+        render(newMessages.get(userName), userName);
+    }
+    $('#selectedUserId').html('');
+    $('#selectedUserId').append('Chat with ' + userName);
+}
+
+function fetchAll() {
+    $.get(url + "/fetchAllUsers", function (response) {
+        let users = response;
+        let usersTemplateHTML = "";
+        for (let i = 0; i < users.length; i++) {
+            usersTemplateHTML = usersTemplateHTML + '<a href="#" onclick="selectUser(\'' + users[i] + '\')"><li class="clearfix">\n' +
+                '                <div class="about">\n' +
+                '                    <div id="userNameAppender_' + users[i] + '" class="name">' + users[i] + '</div>\n' +
+                '                    <div class="status">\n' +
+                '                    </div>\n' +
+                '                </div>\n' +
+                '            </li></a>';
+        }
+        $('#usersList').html(usersTemplateHTML);
+    });
+}
+function init() {
+    cacheDOM();
+    bindEvents();
+}
+
+function bindEvents() {
+    $button.on('click', addMessage.bind(this));
+    $textarea.on('keyup', addMessageEnter.bind(this));
+}
+
+function cacheDOM() {
+    $chatHistory = $('.chat-history');
+    $button = $('#sendBtn');
+    $textarea = $('#message-to-send');
+    $chatHistoryList = $chatHistory.find('ul');
+}
+
+function render(message, userName) {
+    scrollToBottom();
+    // responses
+    var templateResponse = Handlebars.compile($("#message-response-template").html());
+    var contextResponse = {
+        response: message,
+        time: getCurrentTime(),
+        userName: userName
+    };
+
+    setTimeout(function () {
+        $chatHistoryList.append(templateResponse(contextResponse));
+        scrollToBottom();
+    }.bind(this), 1500);
+}
+
+function sendMessage(message) {
+    let username = $('#userName').val();
+    console.log(username)
+    sendMsg(username, message);
+    scrollToBottom();
+    if (message.trim() !== '') {
+        var template = Handlebars.compile($("#message-template").html());
+        var context = {
+            messageOutput: message,
+            time: getCurrentTime(),
+            toUserName: selectedUser
+        };
+
+        $chatHistoryList.append(template(context));
+        scrollToBottom();
+        $textarea.val('');
+    }
+}
+
+function scrollToBottom() {
+    $chatHistory.scrollTop($chatHistory[0].scrollHeight);
+}
+
+function getCurrentTime() {
+    return new Date().toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+}
+
+function addMessage() {
+    sendMessage($textarea.val());
+}
+
+function addMessageEnter(event) {
+    // enter was pressed
+    if (event.keyCode === 13) {
+        addMessage();
+    }
+}
+
+init();
+//Need to create a list with username and chatHistory
+//https://codepen.io/tamnv90/pen/yLyKeoj
