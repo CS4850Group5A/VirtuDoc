441ae882cbef255a71b702edac302f6bebd7330f
Joshua Zenn <wordman05@gmail.com>
2022-03-17 20:34:41 -0400

    PDF export

--- src/main/java/com/virtudoc/web/controller/appointmentController.java
+++ src/main/java/com/virtudoc/web/controller/appointmentController.java
@@ -4,6 +4,8 @@ package com.virtudoc.web.controller;
 import com.lowagie.text.DocumentException;
 import com.virtudoc.web.entity.Appointment;
 import com.virtudoc.web.service.AppointmentServiceCreation;
+import com.virtudoc.web.service.FileService;
+import com.virtudoc.web.service.PDFGeneratorService;
 import org.bouncycastle.cert.ocsp.Req;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,14 +18,23 @@ import org.xhtmlrenderer.pdf.ITextRenderer;
 import org.thymeleaf.context.Context;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.nio.file.FileSystems;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping(path = "/appointment")
 public class appointmentController {
 	@Autowired
 	private AppointmentServiceCreation appointmentService;
+
+	@Autowired
+	private PDFGeneratorService pdfGeneratorService;
+
+	@Autowired
+	private FileService fileService;
 
 	//@RequestMapping(value = "/", method = RequestMethod.GET)
 	@GetMapping("/show")
@@ -36,8 +47,13 @@ public class appointmentController {
 
 	//@RequestMapping(value = "/", method = RequestMethod.POST)
 	@PostMapping("/new")
-	public String createAppointment(Model model, @ModelAttribute Appointment appointment) {
+	public String createAppointment(Model model, @ModelAttribute Appointment appointment) throws Exception {
 		Appointment apt = appointmentService.createAppointment(appointment);
+		Map<String, Object> pdfModel = new HashMap<>();
+		pdfModel.put("appointment", apt);
+		ByteArrayOutputStream outputStream = (ByteArrayOutputStream)pdfGeneratorService.generatePDF(pdfModel, "test/pdftest.html");
+		InputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+		fileService.CreateFile(inputStream);
 		return "redirect:/appointment/show";
 	}
 }
