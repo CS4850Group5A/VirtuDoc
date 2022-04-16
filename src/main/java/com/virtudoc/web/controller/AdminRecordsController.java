731c2d776c5651530f2fab1548420a8a47296acc
Joshua Zenn <wordman05@gmail.com>
2022-04-16 18:53:10 -0400

    WIP files pages

--- src/main/java/com/virtudoc/web/controller/AdminRecordsController.java
+++ src/main/java/com/virtudoc/web/controller/AdminRecordsController.java
@@ -1,79 +1,70 @@
 package com.virtudoc.web.controller;
 
 import java.io.IOException;
-import java.util.stream.Collectors;
 
-import com.google.common.net.HttpHeaders;
-import com.virtudoc.web.uploadingfiles.storage.StorageFileNotFoundException;
-import com.virtudoc.web.uploadingfiles.storage.StorageService;
+import com.virtudoc.web.repository.FileRepository;
+import com.virtudoc.web.service.FileService;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.virtudoc.web.uploadingfiles.storage.StorageFileNotFoundException;
-import com.virtudoc.web.uploadingfiles.storage.StorageService;
+
 
 
 @Controller
 public class AdminRecordsController {
 
-    private final StorageService storageService;
+    @Autowired
+	private FileService fileService;
+
+	@Autowired
+	private FileRepository fileRepository;
 
     @GetMapping("/admin_records")
     public String getMessagePage(){
         return "admin_records";
     }
 
-    @Autowired
-	public FileUploadController(StorageService storageService) {
-		this.storageService = storageService;
-	}
+//    @Autowired
+//	public void FileUploadController(StorageService storageService) {
+//		this.storageService = storageService;
+//	}
 
-	@GetMapping("/")
+	@GetMapping("/files")
 	public String listUploadedFiles(Model model) throws IOException {
 
-		model.addAttribute("files", storageService.loadAll().map(
-				path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-						"serveFile", path.getFileName().toString()).build().toUri().toString())
-				.collect(Collectors.toList()));
+		model.addAttribute("files", fileRepository.findAll());
 
-		return "uploadForm";
+		return "records";
 	}
 
-	@GetMapping("/files/{filename:.+}")
-	@ResponseBody
-	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+//	@GetMapping("/files/{filename:.+}")
+//	@ResponseBody
+//	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+//
+//		Resource file = storageService.loadAsResource(filename);
+//		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+//				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
+//	}
+//
+//	@PostMapping("/")
+//	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+//			RedirectAttributes redirectAttributes) {
+//
+//		storageService.store(file);
+//		redirectAttributes.addFlashAttribute("message",
+//				"You successfully uploaded " + file.getOriginalFilename() + "!");
+//
+//		return "redirect:/";
+//	}
 
-		Resource file = storageService.loadAsResource(filename);
-		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
-	}
-
-	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file,
-			RedirectAttributes redirectAttributes) {
-
-		storageService.store(file);
-		redirectAttributes.addFlashAttribute("message",
-				"You successfully uploaded " + file.getOriginalFilename() + "!");
-
-		return "redirect:/";
-	}
-
-	@ExceptionHandler(StorageFileNotFoundException.class)
-	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-		return ResponseEntity.notFound().build();
-	}
+//	@ExceptionHandler(StorageFileNotFoundException.class)
+//	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+//		return ResponseEntity.notFound().build();
+//	}
 }
